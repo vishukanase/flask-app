6c8cb8b63330bc470bac9fd6f01d59ca9c110a52
vishvajit <vishvajitkanase@gmail.com>
2025-05-29 10:20:21 +0530

    changed

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Good Day 29/05/2025 '
+    return 'Good Day 10.20 '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
