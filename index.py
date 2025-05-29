f456f78ad4cd8c79caad75c24bed783839a34d64
vishukanase <127315232+vishukanase@users.noreply.github.com>
2025-05-29 15:21:13 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Good Day 12.42 '
+    return 'Good Day 15.21 '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
